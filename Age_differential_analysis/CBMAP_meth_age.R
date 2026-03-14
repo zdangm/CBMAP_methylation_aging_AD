3572ea2aa0a3d6a24304180056c42ad43339f979
ZhaiWenli <zwenli@zju.edu.cn>
2026-03-14 14:55:14 +0800

    Update CBMAP_meth_age.R

--- Age_differential_analysis/CBMAP_meth_age.R
+++ Age_differential_analysis/CBMAP_meth_age.R
@@ -4,30 +4,43 @@ library(dplyr)
 
 load('CBMAP_pheno.RData')
 # methylation matrix
-Mdat <- h5read("final_DNAm_Mdat.h5",'Mdat')
-colnames(Mdat) = h5read("final_DNAm_Mdat.h5",'colnames')
-rownames(Mdat) = h5read("final_DNAm_Mdat.h5",'rownames')
-cg <- 2^Mdat / (2^Mdat + 1)
-cg <- t(cg)
+cg <- h5read("final_DNAm_invMdat.h5",'Mdat')
+colnames(cg) = h5read("final_DNAm_invMdat.h5",'colnames')
+rownames(cg) = h5read("final_DNAm_invMdat.h5",'rownames')
 
-sample = intersect(rownames(cg), rownames(pheno_df)) 
+cell.pro = read.table('CBMAP_all_sample_methylation_ctp_with_clr_deconvolution.txt',header=T)
+cell_aligned <- cell.pro[pheno_df$barcode_id, , drop = FALSE]
+pheno_df <- cbind(pheno_df, cell_aligned)
+
+sample = intersect(rownames(cg), rownames(pheno_df)) # 1031 samples
 cg <- cg[sample,]
 pheno_df <- pheno_df[sample,]
 pheno_df$sex_male <- as.factor(pheno_df$sex_male)
 pheno_df$Sample_Plate <- as.factor(pheno_df$Sample_Plate)
+pheno_df$bank <- as.factor(pheno_df$bank)
+pheno_df[] <- lapply(pheno_df, function(x)
+  if (is.numeric(x)) replace(x, is.na(x), median(x, na.rm = TRUE)) else x
+)
+
+# filter hc samples
+all_sample_info <- as.data.frame(read.csv("CBMAP_sample_info.csv",header=T)) 
+selected_sample = all_sample_info$id[which(all_sample_info$diag_dementia == 1)]
+pheno_df = pheno_df[which(! pheno_df$Sample_Name %chin% selected_sample),]
+# pheno_df = pheno_df[which(pheno_df$age > 60),]
+cg = cg[pheno_df$Sample_Name,]
 identical(rownames(pheno_df), rownames(cg))
 
 # Linear regression by cpgs Methylation
 run_linear_regression <- function(methylation_data, covariates) {
   data <- data.frame(methylation_level = methylation_data, covariates)
-  model <- lm(methylation_level ~ age + sex_male + NeuN_pos + Sample_Plate, data = data)
+  model <- lm(methylation_level ~ age + sex_male + bank + PMD + RIN + Exc + Inh + NonN_Astro_FGF3R + NonN_Endo + NonN_Micro + NonN_Oligo_MBP + NonN_OPC + Sample_Plate, data = data)
   summary_model <- summary(model)
   return(summary_model$coefficients[2,])
 }
 analyze_methylation <- function(cg, pheno_df) {
   results <- apply(cg, 2, function(x) {
     methylation_data <- x  
-    covariates <- pheno_df %>% select(age, sex_male, NeuN_pos, Sample_Plate)  
+    covariates <- pheno_df %>% select(age, sex_male, bank, PMD, RIN, Exc, Inh, NonN_Astro_FGF3R, NonN_Endo, NonN_Micro, NonN_Oligo_MBP, NonN_OPC, Sample_Plate)  
     result <- run_linear_regression(methylation_data, covariates)  
     return(result)
   })
@@ -40,4 +53,4 @@ colnames(res) <- c("Estimate", "StdErr", "pValue")
 res$cpg <- colnames(results)
 res <- res[,c("cpg", "Estimate", "StdErr", "pValue")]
 res$fdr <- p.adjust(res$pValue,'BH')
-save(res, file='CBMAP_meth_age.RData')
+save(res, file='CBMAP_meth_age_no_dementia.RData')
