970de3271f8871fb00d9cde93d6329a174254828
ZhaiWenli <zwenli@zju.edu.cn>
2026-03-14 15:31:40 +0800

    Update Hannum_meth_age.R

--- Age_differential_analysis/Hannum_meth_age.R
+++ Age_differential_analysis/Hannum_meth_age.R
@@ -3,6 +3,8 @@ library(dplyr)
 library(rhdf5)
 
 load('/data/GSE40279/processed/pheno_info.RData')
+pheno_df$AGE <- as.numeric(pheno_df$AGE)
+pheno_df = pheno_df[which(pheno_df$AGE > 60),]
 rownames(pheno_df) <- pheno_df$sampleID
 cg <- h5read("/data/GSE40279/processed/final_DNAm_invMdat.h5",'Mdat')
 colnames(cg) = h5read("/data/GSE40279/processed/final_DNAm_invMdat.h5",'colnames')
@@ -11,23 +13,23 @@ rownames(cg) = h5read("/data/GSE40279/processed/final_DNAm_invMdat.h5",'rownames
 sample = intersect(rownames(cg), rownames(pheno_df)) 
 cg <- cg[sample,]
 pheno_df <- pheno_df[sample,]
-pheno_df$AGE <- as.numeric(pheno_df$AGE)
 pheno_df$sex_male <- ifelse(pheno_df$Gender == 'M', 1, 0)
 pheno_df$sex_male <- as.factor(pheno_df$sex_male)
 pheno_df$Sample_Plate <- as.factor(pheno_df$Plate)
+pheno_df$Ethnicity <- as.factor(pheno_df$Ethnicity)
 identical(rownames(pheno_df), rownames(cg))
 
 # Linear regression by cpgs Methylation
 run_linear_regression <- function(methylation_data, covariates) {
   data <- data.frame(methylation_level = methylation_data, covariates)
-  model <- lm(methylation_level ~ AGE + sex_male + Sample_Plate + B + NK + CD4T + CD8T + Mono + Neutro + Eosino, data = data)
+  model <- lm(methylation_level ~ AGE + sex_male + Sample_Plate + Ethnicity + B + NK + CD4T + CD8T + Mono + Neutro + Eosino, data = data)
   summary_model <- summary(model)
   return(summary_model$coefficients[2,])
 }
 analyze_methylation <- function(cg, pheno_df) {
   results <- apply(cg, 2, function(x) {
     methylation_data <- x  
-    covariates <- pheno_df %>% select(AGE, sex_male, Sample_Plate, B, NK, CD4T, CD8T, Mono, Neutro, Eosino)  
+    covariates <- pheno_df %>% select(AGE, sex_male, Sample_Plate, Ethnicity, B, NK, CD4T, CD8T, Mono, Neutro, Eosino)  
     result <- run_linear_regression(methylation_data, covariates)  
     return(result)
   })
@@ -41,4 +43,3 @@ res$cpg <- colnames(results)
 res <- res[,c("cpg", "Estimate", "StdErr", "pValue")]
 res$fdr <- p.adjust(res$pValue,'BH')
 save(res, file='Hannum_meth_age.RData')
-
